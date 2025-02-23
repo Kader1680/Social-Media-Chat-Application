245abbe81674161c8e3242f3a5945ec37c5b2440
Kader1680 <ouldhenniabaghdad@gmail.com>
2025-02-23 19:25:40 +0100

    build 1 socket

--- server/server.js
+++ server/server.js
@@ -1,28 +1,35 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
 
-const http = require("http")
-const express = require("express");
 const app = express();
 const server = http.createServer(app);
 
-// inisilize socket
-const socketIO = require('socket.io');
-const io = socketIO(server)
+// Enable CORS to allow frontend to connect
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173", // Allow frontend connection
+        methods: ["GET", "POST"]
+    }
+});
 
+// Handle WebSocket connections
 io.on('connection', (socket) => {
+    console.log('New client connected:', socket.id);
 
-console.log('New client connected')
+    // Receive message from client and broadcast to all
+    socket.on('message', (message) => {
+        console.log('Message received:', message);
+        io.emit('message', message);  // Broadcast to all clients
+    });
 
-})
+    socket.on('disconnect', () => {
+        console.log('Client disconnected:', socket.id);
+    });
+});
 
-
-
-app.get("/", (req, res)=>{
-
-    res.send("rfdfd")
-})
-
-server.listen(3000, (req, res)=>{
-   
-    
-    console.log("Server is running on port 3000")
-})
+// Start server on port 3000
+server.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});
