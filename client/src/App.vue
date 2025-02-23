fed7b1b23d762a15fb145d2d1ec45d2edade5300
Kader1680 <ouldhenniabaghdad@gmail.com>
2025-02-23 12:48:12 +0100

    build message page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,22 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import Navbar from "./components/navbar.vue"; // Import your Navbar.vue
+
+ 
+
 </script>
 
 <template>
+ 
+  <Navbar />
+    
   <RouterView />
 </template>
+
+<script>
+
+</script>
+
 
 <style scoped></style>
--- server/server.js
+++ server/server.js
@@ -4,9 +4,25 @@ const express = require("express");
 const app = express();
 const server = http.createServer(app);
 
+// inisilize socket
+const socketIO = require('socket.io');
+const io = socketIO(server)
 
+io.on('connection', (socket) => {
+
+console.log('New client connected')
+
+})
+
+
+
+app.get("/", (req, res)=>{
+
+    res.send("rfdfd")
+})
 
 server.listen(3000, (req, res)=>{
+   
     
     console.log("Server is running on port 3000")
 })
